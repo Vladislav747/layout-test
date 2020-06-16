2334561883c8e082ed9512a4dd9e4db1f85db8aa
Vladislav <zippo585@gmail.com>
2020-06-17 00:42:31 +0500

    Updates

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Обработчик меню иконки
+let user_icon = document.querySelector('.user-header__icon');
+
+user_icon.addEventListener("click", function(e){
+    let user_menu = document.querySelector('.user-header__menu');
+    user_menu.classList.toggle('active');
+});
